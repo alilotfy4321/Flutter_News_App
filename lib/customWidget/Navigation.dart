6b506f0cc4e27d24fe5173d8f4986bb7e3d9b7a9
alilotfy4321 <alilotfy4321@gmail.com>
2024-03-15 11:38:28 +0200

    modifies custom widget navigate to

--- lib/customWidget/Navigation.dart
+++ lib/customWidget/Navigation.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/view/newsappScreen/searchScreen.dart';
 
-navigateTo(context, Widget) {
+void navigateTo(context, Widget) {
   Navigator.push(
     context,
     MaterialPageRoute(builder: (context) => Widget),
